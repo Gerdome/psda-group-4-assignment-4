9d7620dc91bde36332e580da5324248308287ab0
Robin Schnaidt <rs@robinschnaidt.com>
2020-07-18 21:58:31 +0200

    Remove further columns from data sets

--- covid-data/dataConversion.py
+++ covid-data/dataConversion.py
@@ -11,7 +11,7 @@ df = pd.read_csv(argv[1])
 print(df)
 
 # Drop columns not needed
-df = df.drop(['V1', 'allbed_lower','allbed_upper', 'ICUbed_lower', 'ICUbed_upper', 'InvVen_lower','InvVen_upper', 'admis_lower', 'admis_upper', 'newICU_lower', 'newICU_upper', 'bedover_lower', 'bedover_upper', 'icuover_lower', 'icuover_upper', 'deaths_lower', 'deaths_upper', 'totdea_lower', 'totdea_upper', 'deaths_mean_smoothed', 'deaths_lower_smoothed', 'deaths_upper_smoothed', 'totdea_mean_smoothed', 'totdea_lower_smoothed', 'totdea_upper_smoothed', 'est_infections_lower', 'est_infections_upper', 'mobility_data_type', 'total_tests_data_type'], axis=1)
+df = df.drop(['V1', 'location_id', 'allbed_lower','allbed_upper', 'ICUbed_lower', 'ICUbed_upper', 'InvVen_lower','InvVen_upper', 'admis_lower', 'admis_upper', 'newICU_lower', 'newICU_upper', 'bedover_lower', 'bedover_upper', 'icuover_lower', 'icuover_upper', 'deaths_lower', 'deaths_upper', 'totdea_lower', 'totdea_upper', 'deaths_mean_smoothed', 'deaths_lower_smoothed', 'deaths_upper_smoothed', 'totdea_mean_smoothed', 'totdea_lower_smoothed', 'totdea_upper_smoothed', 'est_infections_lower', 'est_infections_upper', 'mobility_data_type', 'total_tests_data_type'], axis=1)
 
 # Create data frame copies and filter for countries
 df_ger = df[df.location_name == "Germany"]
@@ -23,7 +23,12 @@ df_ger = df_ger[df_ger.date <= "2020-07-14"]
 df_it = df_it[df_it.date <= "2020-07-14"]
 df_us = df_us[df_us.date <= "2020-07-14"]
 
+# Also drop columns no longer needed now
+df_ger = df_ger.drop(['location_name', 'date'], axis=1)
+df_it = df_it.drop(['location_name', 'date'], axis=1)
+df_us = df_us.drop(['location_name', 'date'], axis=1)
+
 # Write new file
-df_ger.to_csv("covid-ger.csv")
-df_it.to_csv("covid-it.csv")
-df_us.to_csv("covid-us.csv")
+df_ger.to_csv("covid-ger.csv", index=False)
+df_it.to_csv("covid-it.csv", index=False)
+df_us.to_csv("covid-us.csv", index=False)
